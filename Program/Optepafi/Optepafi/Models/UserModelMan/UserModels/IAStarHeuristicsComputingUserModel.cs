7dedcc1c19920d77121fd3e7800532cd5fc8b37f
Matej Kukurugya <matej.kukurugya@gmail.com>
2024-06-15 18:47:43 +0200

    Doplnujuci commit ku predoslemu. Doplneny IAStarHeuristicsComputingUserModel.

---
+++ Program/Optepafi/Optepafi/Models/UserModelMan/UserModels/IAStarHeuristicsComputingUserModel.cs
@@ -0,0 +1,24 @@
+using Optepafi.Models.TemplateMan;
+using Optepafi.Models.TemplateMan.TemplateAttributes;
+
+namespace Optepafi.Models.UserModelMan.UserModels;
+
+/// <summary>
+/// Represents user model that is able to compute A* heuristics for provided vertex attributes. This ability is specific need of A* searching algorithm.
+/// </summary>
+/// <typeparam name="TTemplate">Associated template type.</typeparam>
+/// <typeparam name="TVertexAttributes">Type of vertex attributes, which is user model able to use for computing of the heuristics.</typeparam>
+/// <typeparam name="TEdgeAttributes">Type of edge attributes associated with <c>TTemplate</c>.</typeparam>
+public interface
+    IAStarHeuristicsComputingUserModel<out TTemplate, in TVertexAttributes, in TEdgeAttributes> : IComputingUserModel<TTemplate, TVertexAttributes, TEdgeAttributes>
+    where TTemplate : ITemplate<TVertexAttributes, TEdgeAttributes>
+    where TVertexAttributes : IVertexAttributes where TEdgeAttributes : IEdgeAttributes
+{
+    /// <summary>
+    /// Returns weight of heuristics between two vertices represented by their attributes.
+    /// </summary>
+    /// <param name="from">Origin vertex attributes of computed heuristics.</param>
+    /// <param name="to">Destination vertex attributes of computed heuristics.</param>
+    /// <returns>Value of heuristics.</returns>
+    int GetWeightFromHeuristics(TVertexAttributes from, TVertexAttributes to);
+}
